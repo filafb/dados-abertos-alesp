09c888b05ebf4f8a22f0d97b14be2ccc495a96d3
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-08-13 10:21:31 -0400

    basic server

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const { db } = require('./db')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const createApp = () => {
+  app.use(morgan('dev'))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({extended: true}))
+
+  app.use(express.static(path.join(__dirname, '../public')))
+
+  app.use('/api', require('./api'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'))
+  })
+
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error')
+  })
+
+}
+
+const startListening = () => {
+  app.listen(PORT, () => console.log(`Shaking up on port ${PORT}`))
+}
+
+const syncDB = () => db.sync({force: true})
+
+async function bootApp() {
+  await syncDB()
+  await createApp()
+  await startListening()
+}
+
+bootApp()
