5d3d4eaa6917f619349fcbd91cc01ebf3737eb0d
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-08-13 16:12:36 -0400

    added route to parse and update deputados

--- server/api/routes/upload.js
+++ server/api/routes/upload.js
@@ -6,11 +6,14 @@ const { fork } = require('child_process')
 const path = require('path')
 const fs = require('fs')
 const appDirectory = fs.realpathSync(process.cwd())
+const xmlParserFolder = 'xmlParser'
 
 module.exports = router
 
-router.post('/comissoes', upload.single('file'), (req, res, next) => {
-  const parserComissoes = fork(path.join(appDirectory, 'xmlParser', 'comissoes.js'))
+router.use(upload.single('file'))
+
+router.post('/comissoes', (req, res, next) => {
+  const parserComissoes = fork(path.join(appDirectory, xmlParserFolder, 'comissoes.js'))
   parserComissoes.send({file: req.file.buffer.toString()})
   parserComissoes.on('message', ({created, error}) => {
     parserComissoes.kill()
@@ -23,4 +26,17 @@ router.post('/comissoes', upload.single('file'), (req, res, next) => {
   })
 })
 
+router.post('/deputados', (req, res, next) => {
+  const parserDeputados = fork(path.join(appDirectory, xmlParserFolder, 'deputados.js'))
+  parserDeputados.send({file: req.file.buffer.toString()})
+  parserDeputados.on('message', ({created, error}) => {
+    parserDeputados.kill()
+    if(error) {
+      error.message = 'Could not update db'
+      next(error)
+    } else {
+      res.status(201).json({created})
+    }
+  })
+})
 
---
+++ xmlParser/deputados.js
@@ -0,0 +1,17 @@
+const parseFile = require('./')
+const { Deputados } = require('../server/db/models')
+
+process.on('message', async ({ file }) => {
+  try {
+    const parsedXML = await parseFile(file)
+    const deputados = parsedXML.Deputados.Deputado.map(async deputado => {
+      await Deputados.create(deputado)
+    })
+    await Promise.all(deputados)
+    const countDeputados = deputados.length
+    process.send({created: countDeputados})
+  } catch (e) {
+    console.log(e)
+    process.send({error: e})
+  }
+})
