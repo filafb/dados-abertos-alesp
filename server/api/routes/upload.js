48a9d260a295ab3ab48dc6f1391a7d201af64f6c
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-08-13 16:23:42 -0400

    refactory upload routes to make it reusable for different files

--- server/api/routes/upload.js
+++ server/api/routes/upload.js
@@ -12,25 +12,13 @@ module.exports = router
 
 router.use(upload.single('file'))
 
-router.post('/comissoes', (req, res, next) => {
-  const parserComissoes = fork(path.join(appDirectory, xmlParserFolder, 'comissoes.js'))
+
+router.post('/:arquivo', (req, res, next) => {
+  const { arquivo } = req.params
+  const parserComissoes = fork(path.join(appDirectory, xmlParserFolder, `${arquivo}.js`))
   parserComissoes.send({file: req.file.buffer.toString()})
   parserComissoes.on('message', ({created, error}) => {
     parserComissoes.kill()
-    if(error) {
-      error.message = 'Could not update db'
-      next(error)
-    } else {
-      res.status(201).json({created})
-    }
-  })
-})
-
-router.post('/deputados', (req, res, next) => {
-  const parserDeputados = fork(path.join(appDirectory, xmlParserFolder, 'deputados.js'))
-  parserDeputados.send({file: req.file.buffer.toString()})
-  parserDeputados.on('message', ({created, error}) => {
-    parserDeputados.kill()
     if(error) {
       error.message = 'Could not update db'
       next(error)
