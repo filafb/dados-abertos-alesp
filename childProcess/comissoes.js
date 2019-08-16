dc6d144f19d11820e14b66278581b48cfb319b2f
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-08-16 17:11:29 -0400

    added a filter to comissoes to get only the ones that started in this legislatura or are not closed yet

--- childProcess/comissoes.js
+++ childProcess/comissoes.js
@@ -1,14 +1,21 @@
 const parseFile = require('./')
 const { Comissoes } = require('../server/db/models')
 
+const legislaturaBeginning = new Date(2019, 2, 15)
+
 process.on('message', async ({file}) => {
   try {
     const parsedXML = await parseFile(file)
-    const comissoes = parsedXML.Comissoes.Comissao.map( comissao => {
-      return Comissoes.create(comissao)
-    })
-    await Promise.all(comissoes)
-    const countComissoes = comissoes.length
+    const allComissoes = parsedXML.Comissoes.Comissao
+    const promisesComissoes = []
+    for( let i = 0; i < allComissoes.length; i++) {
+      const DataFimComissao = allComissoes[i].DataFimComissao ? new Date(allComissoes[i].DataFimComissao[0]) : null
+      if(legislaturaBeginning < DataFimComissao || !DataFimComissao ) {
+        promisesComissoes.push(Comissoes.create(allComissoes[i]))
+      }
+    }
+    await Promise.all(promisesComissoes)
+    const countComissoes = promisesComissoes.length
     process.send({created: countComissoes})
 
   } catch (e) {
