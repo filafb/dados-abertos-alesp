36d56d5b0c4ed8328fb980749ef1ddaf002adb22
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-08-16 13:57:46 -0400

    added new fileToUpload obj plus refactored main component

--- client/Main.js
+++ client/Main.js
@@ -1,15 +1,42 @@
-import React from 'react'
-import Upload from './Upload'
+import React from 'react';
+import Upload from './Upload';
 
-function Main () {
+const filesToUpload = [
+  {
+    id: 1,
+    apiLocation: 'comissoes',
+    text: 'Comissões Permanentes',
+  },
+  {
+    id: 2,
+    apiLocation: 'deputados',
+    text: 'Deputados',
+  },
+  {
+    id: 3,
+    apiLocation: 'comissoes_membros',
+    text: 'Comissões Membros',
+  },
+  {
+    id: 4,
+    apiLocation: 'comissoes_permanentes_reunioes',
+    text: 'Reuniões das Comissões',
+  },
+  {
+    id: 5,
+    apiLocation: 'comissoes_permanentes_presencas',
+    text: 'Presenças em comissões',
+  },
+];
+
+function Main() {
   return (
     <React.Fragment>
-      <Upload apiLocation='comissoes' text='Comissões Permanentes' />
-      <Upload apiLocation='deputados' text='Deputados' />
-      <Upload apiLocation='comissoes_membros' text='Comissões Membros' />
-      <Upload apiLocation='comissoes_permanentes_reunioes' text='Reuniões das Comissões' />
+      {filesToUpload.map(({apiLocation, text, id}) => (
+        <Upload apiLocation={apiLocation} text={text} key={id} />
+      ))}
     </React.Fragment>
-  )
+  );
 }
 
-export default Main
+export default Main;
