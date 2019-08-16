ed81f1a159a72608bfbb16f62a925072091b4045
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-08-16 17:17:03 -0400

    filtering comissoes that belong to the current legislatura and deputados that are still on the comissao or left it after the current legislatura started

--- childProcess/comissoes_membros.js
+++ childProcess/comissoes_membros.js
@@ -1,6 +1,7 @@
 const parseFile = require('./')
-const { ComissoesMembros, Deputados } = require('../server/db/models')
+const { ComissoesMembros, Deputados, Comissoes } = require('../server/db/models')
 
+const legislaturaBeginning = new Date(2019, 2, 15)
 
 process.on('message', async ({file}) => {
   try {
@@ -8,10 +9,13 @@ process.on('message', async ({file}) => {
     const allComissoesMembros = parsedXML.ComissoesMembros.MembroComissao
     let promisesComissoesMembros = []
     const getDeputadosIdSPL = await Deputados.findAll({attributes: ['IdSPL']}).map(id => id.IdSPL)
-    //Filtrando deputados ativos. Arquivo original traz comissoes com deputados de outra legislatura, mas lista de deputados só traz os deputados desta legislatura. Como há associação entre as tabelas, necessário o filtro
+    const comissoesLegislaturaAtual = await Comissoes.findAll({attributes: ['IdComissao']}).map(id => id.IdComissao)
+    //Filtrando deputados ativos e comissoes na legislatura atual. Arquivo original traz comissoes com deputados de outra legislatura, mas lista de deputados só traz os deputados desta legislatura. Como há associação entre as tabelas, necessário o filtro
     for( let i = 0; i < allComissoesMembros.length; i++) {
+      const comissaoId = Number(allComissoesMembros[i].IdComissao[0])
       const id = Number(allComissoesMembros[i].IdMembro[0])
-      if(getDeputadosIdSPL.indexOf(id) !== -1) {
+      const dataFimParticipacao = allComissoesMembros[i].DataFim ? new Date(allComissoesMembros[i].DataFim) : null
+      if(comissoesLegislaturaAtual.indexOf(comissaoId) !== -1 && getDeputadosIdSPL.indexOf(id) !== -1 && (dataFimParticipacao > legislaturaBeginning || !dataFimParticipacao)) {
         promisesComissoesMembros.push(ComissoesMembros.create(allComissoesMembros[i]))
       }
     }
