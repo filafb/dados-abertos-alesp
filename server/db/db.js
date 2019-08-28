cb48a5411a27a20cdf10de6a31c6d86582e905b0
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-08-28 12:15:58 -0400

    testing

--- server/db/db.js
+++ server/db/db.js
@@ -4,6 +4,7 @@ const dbName = `dados-abertos${(process.env.NODE_ENV === 'test' ? '-test' : '')}
 
 const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`, { logging: false })
 
+console.log()
 
 module.exports = db
 
