5a6e52ffc18a6bdd3642844e82df0af90ec07760
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-08-28 09:31:04 -0400

    added components to display deputado info plus comissoes

---
+++ client/Comissoes/ParticipaComissoes.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios';
+
+export default function ParticipaComissoes ({deputadoSPL}) {
+  const [comissoes, setComissoes] = useState([])
+
+  useEffect(() => {
+    async function getComissoes(idSpl) {
+      const { data: comissoes } = await Axios.get(`/api/comissoes/membros/${idSpl}`)
+      setComissoes(comissoes)
+    }
+    getComissoes(deputadoSPL)
+  }, [deputadoSPL])
+
+  const { member, wasMember } = comissoes.reduce((finalObj, currentComissao) => {
+    if(currentComissao.DataFim) {
+      finalObj.wasMember.push(currentComissao)
+    } else {
+      finalObj.member.push(currentComissao)
+    }
+    return finalObj
+  }, {member: [], wasMember: []})
+
+  console.log('member', member)
+  console.log('wasMember', wasMember)
+
+  return (
+    <React.Fragment>
+      <div>
+      <h3>Membro de</h3>
+        {member.length && member.map(comissao => {
+          const {IdComissao, DataInicio, Papel, comissao: {NomeComissao}, reunioes } = comissao
+          return (
+            <div key={IdComissao}>
+              <div>{NomeComissao}</div>
+              <div>Desde de: {DataInicio}</div>
+              <div>Papel: {Papel}</div>
+              <ul>
+                {reunioes.map(reuniao => {
+                  return (
+                    <li key={reuniao.IdReuniao}>
+                      <span >Data: {reuniao.Data} - Presença: {reuniao.presenças.length ? 'Sim' : 'Não'}</span>
+                    </li>
+                  )
+                })}
+
+              </ul>
+              <hr />
+            </div>
+          )
+        })}
+      </div>
+      <div>
+        <h3>Também já participou:</h3>
+        {wasMember.length && wasMember.map(comissao => {
+          const {IdComissao, DataInicio, DataFim, Papel, comissao: {NomeComissao}, reunioes } = comissao
+          return (
+            <div key={IdComissao}>
+              <div>{NomeComissao}</div>
+              <div>Desde de: {DataInicio}</div>
+              <div>Até: {DataFim}</div>
+              <div>Papel: {Papel}</div>
+              <ul>
+                {reunioes.map(reuniao => {
+                  return (
+                    <li key={reuniao.IdReuniao}>
+                      <span >Data: {reuniao.Data} - Presença: {reuniao.presenças.length ? 'Sim' : 'Não'}</span>
+                    </li>
+                  )
+                })}
+
+              </ul>
+              <hr />
+            </div>
+          )
+        })}
+      </div>
+    </React.Fragment>
+  )
+}
--- client/Deputados/Deputado.js
+++ client/Deputados/Deputado.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect } from 'react'
 import axios from 'axios'
+import ParticipaComissoes from '../Comissoes/ParticipaComissoes'
 
 export default function Deputado ({match : { params: {id}}}) {
   const [ deputado, setDeputado ] = useState({})
+  const [ openModal, toggleModal ] = useState(false)
   useEffect(() => {
     async function getADeputado(id) {
       const { data: deputado} = await axios.get(`/api/deputados/${id}`)
@@ -10,14 +12,21 @@ export default function Deputado ({match : { params: {id}}}) {
     }
     getADeputado(id)
   }, [id])
-  console.log('props', deputado)
-  const {NomeParlamentar, Partido, PathFoto, Email, Biografia } = deputado
+
+  const toggleComissoes = (e) => {
+    toggleModal(!openModal)
+  }
+  const {NomeParlamentar, Partido, PathFoto, Email, Biografia, IdSPL } = deputado
   return (
     <div>
-      <img style={{width:'190px', borderRadius: '30px'}} src={PathFoto} alt={NomeParlamentar} />
-      <div>Nome: {NomeParlamentar}</div>
-      <div>Partido: {Partido}</div>
-      <div>Email: {Email}</div>
+      <div>
+        <img style={{width:'190px', borderRadius: '30px'}} src={PathFoto} alt={NomeParlamentar} />
+        <div>Nome: {NomeParlamentar}</div>
+        <div>Partido: {Partido}</div>
+        <div>Email: {Email}</div>
+      </div>
+      <button type='button' onClick={toggleComissoes}>Comissões</button>
+      {openModal && <ParticipaComissoes deputadoSPL={IdSPL} />}
     </div>
   )
 }
